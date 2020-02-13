b5f73e85598e32ae66f419805fc2cc70d0db9eaa
Naser Rafinia <Naser.r@outlook.com>
2020-02-13 22:58:49 +0330

    Add priority in IoC.Installer

--- src/F4ST.Queue/Tools/QueueInstaller.cs
+++ src/F4ST.Queue/Tools/QueueInstaller.cs
@@ -6,9 +6,11 @@ namespace F4ST.Queue.Tools
 {
     public class QueueInstaller : IIoCInstaller
     {
+        public int Priority => -79;
         public void Install(WindsorContainer container, IMapper mapper)
         {
 
         }
+
     }
 }
